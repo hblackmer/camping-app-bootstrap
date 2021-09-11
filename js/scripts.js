b84d1d516877f877c0509f4c0a73f3b7315cf0a5
Helene <hcblackmer@gmail.com>
2021-09-11 12:24:44 -0700

    Added modal javascript into one block

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
+$(function () {
+    $(".carousel").carousel({ interval: 2000, pause: "false" });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -8,19 +8,13 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
-});
-
-$(function () {
     $('#reserveButton').click(function () {
-      $('#reserveModal').modal('show');
-    })
-});
-
-$(function () {
+        $('#reserveModal').modal('show');
+    });
     $('#loginButton').click(function () {
-      $('#loginModal').modal('show');
-    })
+        $('#loginModal').modal('show');
+    });
 });
